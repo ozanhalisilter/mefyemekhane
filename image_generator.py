00061741c0468f757842a1fd1cd2bbbc0689c1a7
Ahmet YAMAN <ymna32@gmail.com>
2022-12-21 16:32:49 +0300

    add header text and font

--- image_generator.py
+++ image_generator.py
@@ -6,11 +6,14 @@ class ImageGenerator:
     def __init__(self):
         self.img = Image.open("empty_page.jpg")
         self.font = ImageFont.truetype("arial.ttf",16)
+        self.header_font = ImageFont.truetype("arial.ttf",24)
         self.draw = ImageDraw.Draw(self.img)
 
     def generate_menu(self,text_list):
         coordinates = (150,150)
         color = 2
+        header = "Yemek Listesi"
+        self.draw.text((250,120), header, font=self.header_font, align="center")
         self.draw.text((150,150), text_list, font=self.font)
 
 
