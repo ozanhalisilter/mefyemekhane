9968f7acc4588f7f411db600942769f4bacbdee1
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2022-12-21 16:12:13 +0300

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -1,23 +1,27 @@
 from PIL import Image,ImageFont,ImageDraw
-
+import req
 
 
 class ImageGenerator:
     def __init__(self):
         self.img = Image.open("empty_page.jpg")
-        self.font = ImageFont.truetype("cassandra.ttf",25)
+        self.font = ImageFont.truetype("arial.ttf",16)
         self.draw = ImageDraw.Draw(self.img)
 
-    def generate_menu(self,text):
+    def generate_menu(self,text_list):
         coordinates = (150,150)
         color = 2
-        self.draw.text(coordinates, text, font=self.font)
+        self.draw.text((150,150), text_list, font=self.font)
+
+
         self.img.save("text.png")
 
 
 if __name__ == '__main__':
     generator = ImageGenerator()
-    generator.generate_menu("Hello world!")
+    requester = req.Menu()
+    print(requester.get_menu())
+    generator.generate_menu(requester.get_menu())
 
 
 
